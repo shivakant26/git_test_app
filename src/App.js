83170a1da2ffe7e0a5d10da1b67807bacf9d9c36
shivakant <shivakantyuvasoft357@gmail.com>
2023-10-05 14:23:44 +0530

    add src file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import SiteHeader from './component/layout/Header';
 import AboutPage from './pages/aboutPage';
 import ErrorPage from './pages/errorPage';
 import HomePage from './pages/homePage';
@@ -6,11 +7,11 @@ import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <SiteHeader />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<ErrorPage />} />
-      
       </Routes>
     </div>
   );
