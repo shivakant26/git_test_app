4853fe1a71380c1ad1cfb46e57a83afbc8deac2c
monus97 <monus3871@gmail.com>
2023-10-05 07:16:13 +0530

    working on create pages home,about,error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Home from './component/Home';
-
+import AboutPage from './pages/aboutPage';
+import ErrorPage from './pages/errorPage';
+import HomePage from './pages/homePage';
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/errorPage.js
@@ -0,0 +1,8 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>404 Not found</h1>
+    </div>
+  );
+};
+export default ErrorPage;
